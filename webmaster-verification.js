cc8bead2195157cf02650a02485ef44b1fa4a0c1
Mikhail Aivazyan <me@m1sh4xd.ru>
2026-04-24 17:30:48 +0300

    Update webmaster-verification.js
    Idempotency fix

--- webmaster-verification.js
+++ webmaster-verification.js
@@ -1,45 +1,69 @@
 // Copyright 2026 YANDEX LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the Apache License, Version 2.0
 
 (function (window, document) {
   'use strict';
 
   var config = window.yandexWebmasterVerification || {};
-  var token = typeof config.token === 'string' ?
-      config.token.replace(/^\s+|\s+$/g, '') :
-      '';
-
-  if (!token) {
-    return;
-  }
-
   var head = document.head || document.getElementsByTagName('head')[0];
+
   if (!head) {
     return;
   }
 
-  var meta = head.querySelector('meta[name="yandex-verification"]');
-
-  if (meta && meta.getAttribute('content') === token) {
-    return;
+  function trim(value) {
+    return typeof value === 'string'
+      ? value.replace(/^\s+|\s+$/g, '')
+      : '';
   }
 
-  if (!meta) {
-    meta = document.createElement('meta');
+  function hasMetaWithToken(token) {
+    var metas = head.querySelectorAll('meta[name="yandex-verification"]');
+
+    for (var i = 0; i < metas.length; i += 1) {
+      if (metas[i].getAttribute('content') === token) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function addVerificationMeta(token) {
+    token = trim(token);
+
+    if (!token || hasMetaWithToken(token)) {
+      return;
+    }
+
+    var meta = document.createElement('meta');
     meta.setAttribute('name', 'yandex-verification');
+    meta.setAttribute('content', token);
     head.appendChild(meta);
   }
 
-  meta.setAttribute('content', token);
+  function collectTokens(config) {
+    var tokens = [];
+
+    // Новый формат: { tokens: ['token1', 'token2'] }
+    if (config && Object.prototype.toString.call(config.tokens) === '[object Array]') {
+      for (var i = 0; i < config.tokens.length; i += 1) {
+        tokens.push(config.tokens[i]);
+      }
+    }
+
+    // Обратная совместимость со старым форматом: { token: 'token1' }
+    if (config && typeof config.token === 'string') {
+      tokens.push(config.token);
+    }
+
+    return tokens;
+  }
+
+  var tokens = collectTokens(config);
+
+  for (var j = 0; j < tokens.length; j += 1) {
+    addVerificationMeta(tokens[j]);
+  }
 })(window, document);
